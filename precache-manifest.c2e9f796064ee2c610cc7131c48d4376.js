a089a540725f20a455de18aaf7133fa351a8f62b
William Lee <leewill@bu.edu>
2024-10-28 03:23:27 -0400

    Updates

---
+++ precache-manifest.c2e9f796064ee2c610cc7131c48d4376.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7c578d46f4e92ae21fc336aa7d8228",
+    "url": "/profolio/index.html"
+  },
+  {
+    "revision": "bf92c3bfc2a2090b336f",
+    "url": "/profolio/static/css/main.a41395ef.chunk.css"
+  },
+  {
+    "revision": "d624fe69bf494f26eb09",
+    "url": "/profolio/static/js/2.15fa627e.chunk.js"
+  },
+  {
+    "revision": "09dfcff8991931712817539b48cc9b6c",
+    "url": "/profolio/static/js/2.15fa627e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039580e6e3c47311ff9a",
+    "url": "/profolio/static/js/3.48d1475a.chunk.js"
+  },
+  {
+    "revision": "bf92c3bfc2a2090b336f",
+    "url": "/profolio/static/js/main.4d6bcd6d.chunk.js"
+  },
+  {
+    "revision": "672eee1b631fc834948f",
+    "url": "/profolio/static/js/runtime-main.05012810.js"
+  }
+]);
